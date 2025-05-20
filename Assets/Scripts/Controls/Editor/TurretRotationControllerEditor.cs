2f74a40478e1d71b7acedd1b179222f4843c95fa
Ivan Vanofis Buzynnyi <synergyivanb@gmail.com>
2025-05-20 03:14:21 +0300

    added a rotating artillery

---
+++ Assets/Scripts/Controls/Editor/TurretRotationControllerEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Features.Controls.Editor
+{
+    [CustomEditor(typeof(TurretRotationController))]
+    public class TurretRotationControllerEditor : UnityEditor.Editor
+    {
+        [DrawGizmo(GizmoType.Selected | GizmoType.InSelectionHierarchy)]
+        public static void DrawGizmo(TurretRotationController controller, GizmoType gizmoType)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(controller.EditorTurretPosition, controller.EditorDeadZoneRadius);
+        }
+    }
+}
